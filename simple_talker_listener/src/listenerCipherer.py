3bead8b9efa27ae755011932d1588a7a1468a4ce
UleRobots <jesus.balsa@unileon.es>
2016-12-04 09:46:46 +0100

    Update listenerCipherer.py

--- simple_talker_listener/src/listenerCipherer.py
+++ simple_talker_listener/src/listenerCipherer.py
@@ -21,35 +21,32 @@ INTERRUPT = u'\u0001'
 PAD = u'\u0000'
 
 # Metodo en el que se descifra el mensaje recibido en funcion del metodo seleccionado
-# Se obtiene la SK y el tipo de cifrado, se obtiene el IV de la primera parte del mensaje
-# Se inicializa el cifrador con la SK y el IV
-# Se descifra el resto del mensaje sin tener en cuenta el IV
 def decrypt_chunk(chunk):
     decoded_encrypted_data = chunk
     
-    method_ = rospy.get_param('~ciphering')
-    SECRET_KEY = rospy.get_param('~secret_key')
-    IV = decoded_encrypted_data[:AES.block_size]
+    method_ = rospy.get_param('~ciphering') # get ciphering param: AES (default) or 3DES
+    SECRET_KEY = rospy.get_param('~secret_key') # get secret key
+    IV = decoded_encrypted_data[:AES.block_size] # get IV from received message
     
     if method_ == "AES":
-        encryptor = AES.new(SECRET_KEY, AES.MODE_CBC, IV)
+        encryptor = AES.new(SECRET_KEY, AES.MODE_CBC, IV) # init AES cipher
     elif method_ == "3DES":
-        encryptor = DES3.new(SECRET_KEY, DES3.MODE_CFB, IV)
+        encryptor = DES3.new(SECRET_KEY, DES3.MODE_CFB, IV) # init 3DES cipher
     else:
         rospy.logerr('Invalid encryption method: %s', method_)
     
-    decrypted_data = encryptor.decrypt(decoded_encrypted_data[AES.block_size:])
+    decrypted_data = encryptor.decrypt(decoded_encrypted_data[AES.block_size:]) # Decrypt message (without IV)
     decrypted_stripped = StripPadding(decrypted_data, INTERRUPT, PAD)
     
-    tag_received = decrypted_stripped[:10]
-    tag = "%s" % rospy.get_rostime().secs
+    tag_received = decrypted_stripped[:10] # get tag from received message
+    tag = "%s" % rospy.get_rostime().secs # calculate local tag
     
     rospy.loginfo(rospy.get_caller_id() + ' - tag: %s, tag_received: %s', tag, tag_received)
     
-    if tag != tag_received:
+    if tag != tag_received: # if received tag is not equal to local tag we have finished
         return
     
-    msg = decrypted_stripped[10:]
+    msg = decrypted_stripped[10:] # get message
     
     return msg
 
@@ -65,14 +62,14 @@ def callback(data):
 
 # Metodo en el que se crea un nodo y se suscribe al topic chatter
 def listener():
-    rospy.init_node('listener', anonymous=True)    
-    rospy.Subscriber('chatter', String, callback)
+    rospy.init_node('listener', anonymous=True)     # init node
+    rospy.Subscriber('chatter', String, callback)   # suscribe chatter topic
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
 if __name__ == '__main__':
     #This sleep is needed for synchronization
-    time.sleep(3)
+    time.sleep(1)
     listener()
 
